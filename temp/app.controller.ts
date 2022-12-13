390fb7daa8cd63442d5a2019be533b2752736658
alex-gudkov-innowise <alex.gudkov@innowise-group.com>
2022-12-13 12:25:32 +0300

    Connect to PostgreSQL using Sequelize. GET/POST methods.

--- temp/app.controller.ts
+++ temp/app.controller.ts
@@ -1,16 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller('/api')
 export class AppController
 {
+    // чтобы сервис использовать внутри controller необходимо внедрить (dependency injection)
+    constructor(private appService: AppService)
+    {
+
+    }
+
     // чтобы GetUsers стала endpoint и мы могли отправлять HTTP запрос, пометим ее декоратором
     @Get('/users')
     GetUsers()
     {
-        return [
-            { id: 1, name: 'Alex' },
-            { id: 2, name: 'Stas' },
-        ];
+        return this.appService.GetUsers();
     }
 };
 
